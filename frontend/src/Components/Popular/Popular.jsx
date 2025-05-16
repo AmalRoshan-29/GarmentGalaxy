ef76e135b61e84e634be12c1f8eb31af70203c32
AmalRoshan-29 <145017287+AmalRoshan-29@users.noreply.github.com>
2025-05-16 14:37:43 +0530

    Update Popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
   const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(() => {
-    fetch("https://garmentgalaxy-backend.onrender.com/images/popularinwomen")
+    fetch("https://garmentgalaxy-backend.onrender.com/popularinwomen")
       .then((response) => response.json())
       .then((data) => setPopularProducts(data));
   }, []);
