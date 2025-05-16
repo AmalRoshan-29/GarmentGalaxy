3a3077b1bc29cc94fdfe49de2c135030c7abfc18
AmalRoshan-29 <amalroshanar2002@gmail.com>
2025-05-16 10:15:04 +0530

    Initial commit with MERN stack folders

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import productcart from "../src/assets/Product_Cart.png";
+import Navbar from "./Components/Navbar/Navbar";
+import Admin from "./Pages/Admin/Admin";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Admin />
+    </div>
+  );
+};
+
+export default App;
