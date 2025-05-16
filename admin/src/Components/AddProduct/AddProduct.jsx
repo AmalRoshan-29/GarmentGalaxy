5f948a5f05fa45a76ea96d1e0496d3e97f7152f0
AmalRoshan-29 <145017287+AmalRoshan-29@users.noreply.github.com>
2025-05-16 12:47:47 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -28,7 +28,7 @@ const AddProduct = () => {
     let formData = new FormData();
     formData.append("product", image);
 
-    await fetch("http://localhost:4000/upload", {
+    await fetch("https://garmentgalaxy-backend.onrender.com/upload", {
       method: "POST",
       headers: {
         Accept: "application/json",
