62b822039b5c0f69bd1c6b2ecaecc1c9c72f7d78
AmalRoshan-29 <145017287+AmalRoshan-29@users.noreply.github.com>
2025-05-16 12:57:04 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,117 +1,3 @@
-// import React from "react";
-// import "./CSS/LoginSignup.css";
-// import { useState } from "react";
-
-// const LoginSignup = () => {
-//   const [state, setState] = useState("Login");
-//   const [formData, setFormData] = useState({
-//     username: "",
-//     password: "",
-//     email: "",
-//   });
-
-//   const changeHandler = (e) => {
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-//   };
-
-//   const login = async () => {
-//     console.log("Login Function Executed", formData);
-//   };
-
-//   const signup = async () => {
-//     console.log("Signup Function Executed", formData);
-//     let responseData;
-//     await fetch("http://localhost:4000/signup", {
-//       method: "POST",
-//       headers: {
-//         Accept: "application/form-data",
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(formData),
-//     })
-//       .then((response) => response.json())
-//       .then((data) => (responseData = data));
-
-//     if (responseData.success) {
-//       localStorage.setItem("auth-token", responseData.token);
-//       window.location.replace("/");
-//     } else {
-//       alert(responseData.errors);
-//     }
-//   };
-
-//   return (
-//     <div className="loginsignup">
-//       <div className="loginsignup-container">
-//         <h1>{state}</h1>
-//         <div className="loginsignup-fields">
-//           {state === "Sign Up" ? (
-//             <input
-//               type="text"
-//               name="username"
-//               value={formData.username}
-//               onChange={changeHandler}
-//               placeholder="Your Name"
-//             />
-//           ) : (
-//             <></>
-//           )}
-//           <input
-//             name="email"
-//             value={formData.email}
-//             onChange={changeHandler}
-//             type="email"
-//             placeholder="Email Address"
-//           />
-//           <input
-//             name="password"
-//             value={formData.password}
-//             onChange={changeHandler}
-//             type="password"
-//             placeholder="password"
-//           />
-//         </div>
-//         <div className="loginsignup-agree">
-//           <input type="checkbox" name="" id="" />
-//           <p>By continuing, i agree to the terms of use & privacy policy.</p>
-//         </div>
-//         <button
-//           onClick={() => {
-//             state === "Login" ? login() : signup();
-//           }}
-//         >
-//           Continue
-//         </button>
-//         {state === "Sign Up" ? (
-//           <p className="loginsignup-login">
-//             Already have an account?{""}
-//             <span
-//               onClick={() => {
-//                 setState("Login");
-//               }}
-//             >
-//               Login
-//             </span>
-//           </p>
-//         ) : (
-//           <p className="loginsignup-login">
-//             Create an account?{" "}
-//             <span
-//               onClick={() => {
-//                 setState("Sign Up");
-//               }}
-//             >
-//               Click here
-//             </span>
-//           </p>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LoginSignup;
-
 import React, { useState } from "react";
 import "./CSS/LoginSignup.css";
 
@@ -147,7 +33,7 @@ const LoginSignup = () => {
     if (!validateForm()) return;
 
     try {
-      const response = await fetch("http://localhost:4000/login", {
+      const response = await fetch("https://garmentgalaxy-backend.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -174,7 +60,7 @@ const LoginSignup = () => {
     if (!validateForm()) return;
 
     try {
-      const response = await fetch("http://localhost:4000/signup", {
+      const response = await fetch("https://garmentgalaxy-backend.onrender.com/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
